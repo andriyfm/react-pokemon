c00948ab1dbed0515a8360f5794992e6b30d4346
andriyfm <andrifmsyh@gmail.com>
2023-06-10 21:11:24 +0700

    style(Badge): change bg color

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,11 +6,11 @@ interface BadgeProps {
 export default function Badge({ text, isOutline }: BadgeProps) {
   const bgColor = isOutline
     ? "bg-transparent border border-slate-500 text-slate-100"
-    : "bg-gray-200";
+    : "bg-slate-600 text-slate-100";
 
   return (
     <span
-      className={`px-2.5 py-1 mx-1 text-[10px] font-semibold uppercase text-gray-500 rounded-full ${bgColor}`}
+      className={`px-2.5 py-1 mx-1 text-[10px] font-semibold uppercase rounded-full ${bgColor}`}
     >
       {text}
     </span>
